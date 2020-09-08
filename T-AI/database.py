50cf0fb140378dfb10e9ceb070e54a2a982c63d6
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2020-09-08 14:54:57 +0100

    database GET working 🎆🎉

--- T-AI/database.py
+++ T-AI/database.py
@@ -6,6 +6,11 @@ cnx = mysql.connector.connect(
     host="tharsus-hackathon-2020.mysql.database.azure.com",
     port=3306, ssl_verify_cert=False)
 
+cur = cnx.cursor()
+cur.execute("SELECT * FROM hackathon.pollutiondata")
+result = cur.fetchall()
 
+for x in result:
+  print(x)
 
 cnx.close()
